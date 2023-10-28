c1653002a26c7873d3f2e858ad201fabab6b7b5e
erickdb <erickdarmawanboeniarto130803@gmail.com>
2023-10-28 17:30:35 +0700

    fix:function logout and function is password used

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,7 @@
 <?php
-function is_password_used($pdo, $password) 
+function is_password_used($dbh, $password) 
 {
-    $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE password = :password");
+    $stmt = $dbh->prepare("SELECT COUNT(*) FROM users WHERE password = :password");
     $stmt->execute(array(':password' => $password));
     $count = $stmt->fetchColumn();
     return $count > 0;
